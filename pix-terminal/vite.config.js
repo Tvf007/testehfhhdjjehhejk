5423bb6583b816085e4b405f0c2f362f7f1e20c5
Tvf007 <mepassaseuemailvoce@gmail.com>
2026-04-24 18:46:46 -0300

    Update vite.config.js

--- pix-terminal/vite.config.js
+++ pix-terminal/vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
   }
 })
